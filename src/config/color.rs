3cc9195b520e9c2fa0db9901153eb9efc69fb1a4
Nathan Hüsken <nathan@wintercloud.de>
2022-02-21 11:43:44 +0100

    [impl+test] color from config

---
+++ src/config/color.rs
@@ -0,0 +1,56 @@
+use serde::{Deserialize};
+
+
+/// Color in the configuration.
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(untagged)]
+enum ColorConfig {
+    /// The color, when it is provided as an HEX string (example #FF0000)
+    HEXString(String),
+    /// The color with explicit values for red, green and blue
+    RGB(ColorConfigRGB)
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+struct ColorConfigRGB {
+    red: u8,
+    green: u8,
+    blue: u8
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_color_from_hex_string() {
+        // Setup
+        let hex_value = "#FF0000";
+        let yaml = format!("'{}'", hex_value);
+
+        // Act
+        let deserialize: ColorConfig = serde_yaml::from_str(&yaml).unwrap();
+
+        // Test
+        assert_eq!(deserialize, ColorConfig::HEXString(String::from(hex_value)));
+    }
+
+    #[test]
+    fn test_color_from_rgb() {
+        // Setup
+        let red: u8 = 0;
+        let green: u8 = 1;
+        let blue: u8 = 2;
+        let yaml = format!("red: {}\ngreen: {}\nblue: {}", red, green, blue);
+
+        // Act
+        let deserialize: ColorConfig = serde_yaml::from_str(&yaml).unwrap();
+
+        // Test
+        assert_eq!(deserialize, ColorConfig::RGB(ColorConfigRGB {
+            red,
+            green,
+            blue
+        }));
+    }
+}
---
+++ src/config/mod.rs
@@ -0,0 +1,6 @@
+/// Load configuration file.
+///
+/// See the (example config)[../../doc/example_config.yml].
+
+
+mod color;
